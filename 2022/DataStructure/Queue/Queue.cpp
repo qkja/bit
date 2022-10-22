a644d543df508fed7912d58912f231e97c373c1e
算法杰哥 <qkj1205@163.com>
2022-10-22 10:06:09 +0800

    循环队列

--- 2022/DataStructure/Queue/Queue.cpp
+++ 2022/DataStructure/Queue/Queue.cpp
@@ -1,5 +1,130 @@
 #include "Queue.h"
 
+MyCircularQueue* myCircularQueueCreate(int k) {
+  // 这里要求是 是 k 的有效元素 我们 开辟 k+1
+  MyCircularQueue* m = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+  assert(m);
+  MQDataType*ret = (MQDataType*)malloc(sizeof(MQDataType) * (k+1)) ;
+  assert(ret);
+  m->array = ret;
+  m->front = 0;
+  m->tail = 0;
+  m->cap = k+1;
+  return m;
+}
+
+// 入队列
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+  assert(obj);
+  // 判满
+  if(myCircularQueueIsFull(obj))
+    return false;
+  
+
+
+
+  obj->array[obj->tail] = value;
+  if(obj->tail == obj->cap-1)
+    obj->tail = 0;
+  else 
+    obj->tail++;
+  return true;
+
+  //obj->array[obj->tail] = value;
+  //// 这里修改 tail 
+  //int ret = (obj->tail+1) % (obj->cap);
+  //obj->tail = ret;
+  //return true;
+}
+
+// 出队列
+bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+
+  assert(obj);
+  // 判空
+  if(myCircularQueueIsEmpty(obj))
+    return false;
+  if(obj->front == obj->cap-1)
+    obj->front = 0;
+  else 
+    obj->front++;
+  // 这里修改 front 
+  //int ret = (obj->front+1)%(obj->cap);
+  //obj->front = ret;
+  return true;
+}
+
+// 头部
+int myCircularQueueFront(MyCircularQueue* obj) {
+
+  assert(obj);
+  // 判空
+  if(myCircularQueueIsEmpty(obj))
+    return -1;
+  return obj->array[obj->front];
+}
+
+// 尾部
+int myCircularQueueRear(MyCircularQueue* obj) {
+
+  assert(obj);
+  // 判空
+  if(myCircularQueueIsEmpty(obj))
+    return -1;
+
+  // 如果 tail 是 0 单拿出来
+  if(obj->tail == 0)
+  {
+    return obj->array[obj->cap - 1];
+  }
+  return obj->array[obj->tail - 1];
+}
+
+// 空
+bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
+  assert(obj);
+  return obj->front == obj->tail;
+}
+
+// 满
+bool myCircularQueueIsFull(MyCircularQueue* obj) {
+  assert(obj);
+  size_t ret = (obj->tail+1)%obj->cap;
+  return ret == obj->front;
+}
+
+// 释放空间
+void myCircularQueueFree(MyCircularQueue* obj) {
+  assert(obj);
+  free(obj->array);
+  obj->array = NULL;
+  obj->front = 0;
+  obj->tail = 0;
+  free(obj);
+  // 这里修改obj 改变的是行参
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void QueueInit(Queue* pq)
 {
   assert(pq);
@@ -10,20 +135,30 @@ void QueueInit(Queue* pq)
 void QueueDestory(Queue* pq)
 {
   assert(pq);
-  if(pq->head == NULL)
-    return;
+  //if(pq->head == NULL
+  //    && pq->tail == NULL)
+  //  return;
 
   QueueNode* cur = pq->head;
-  QueueNode* curNext = pq->head->next;
-  while(curNext)
+  while(cur)
   {
+    QueueNode* next = cur->next;
     free(cur);
-    cur = curNext;
-    curNext = curNext->next;
+    cur = next;
   }
-  free(cur);
   pq->head = NULL;
   pq->tail = NULL;
+  //QueueNode* cur = pq->head;
+  //QueueNode* curNext = pq->head->next;
+  //while(curNext)
+  //{
+  //  free(cur);
+  //  cur = curNext;
+  //  curNext = curNext->next;
+  //}
+  //free(cur);
+  //pq->head = NULL;
+  //pq->tail = NULL;
 
   //while(cur)
   //{
@@ -37,22 +172,27 @@ void QueueDestory(Queue* pq)
   //pq->tail = NULL;
 }
 
-QueueNode* BuyQueueNode(QDataType x)
-{
-  QueueNode* node = (QueueNode*)malloc(sizeof(QueueNode));
-  assert(node);
-  node->data = x;
-  node->next = NULL;
-  return node;
-}
+// 没有必要写 只有自己用
+//QueueNode* BuyQueueNode(QDataType x)
+//{
+//  QueueNode* node = (QueueNode*)malloc(sizeof(QueueNode));
+//  assert(node);
+//  node->data = x;
+//  node->next = NULL;
+//  return node;
+//}
 
 void QueuePush(Queue* pq, QDataType x)
 {
   assert(pq);
-  QueueNode* node = BuyQueueNode(x);
-
+  //QueueNode* node = BuyQueueNode(x);
+  QueueNode* node = (QueueNode*)malloc(sizeof(QueueNode));
+  assert(node);
+  node->data = x;
+  node->next = NULL;
   // 第一次插入
-  if(pq->head == NULL)
+  if(pq->head == NULL 
+      && pq->tail == NULL)
     pq->head = node;
   else
     pq->tail->next = node;
@@ -60,13 +200,25 @@ void QueuePush(Queue* pq, QDataType x)
   pq->tail = node;
 }
 
+//void QueuePop(Queue* pq)
+//{
+//  assert(pq);
+//  assert(pq->head);
+//  QueueNode* next = pq->head->next;
+//  free(pq->head);
+//  pq->head = next;
+//}
+
 void QueuePop(Queue* pq)
 {
   assert(pq);
-  assert(pq->head);
+  assert(pq->head && pq->tail);
   QueueNode* next = pq->head->next;
   free(pq->head);
   pq->head = next;
+  // 要一起为空
+  if(next == NULL)
+    pq->tail == NULL:
 }
 
 bool QueueEmpty(Queue* pq)
@@ -77,6 +229,8 @@ bool QueueEmpty(Queue* pq)
 
 size_t QueueSize(Queue* pq)
 {
+  // 这个接口是最慢了
+  // 如果不想遍历 可以使用 一个计数
   assert(pq);
   QueueNode* cur = pq->head;
   size_t count = 0;
@@ -91,7 +245,7 @@ size_t QueueSize(Queue* pq)
 QDataType QueueFront(Queue* pq)
 {
   assert(pq);
-  assert(pq->head);
+  assert(pq->head && pq->tail);
 
   return pq->head->data;
 }
@@ -99,6 +253,6 @@ QDataType QueueFront(Queue* pq)
 QDataType QueueBack(Queue* pq)
 {
   assert(pq);
-  assert(pq->head);
+  assert(pq->head && pq->tail);
   return pq->tail->data;
 }
--- 2022/DataStructure/Queue/Queue.h
+++ 2022/DataStructure/Queue/Queue.h
@@ -1,6 +1,5 @@
 #ifndef __QUEUE_H__
 #define __QUEUE_H__ 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -30,3 +29,47 @@ bool QueueEmpty(Queue* pq);
 size_t QueueSize(Queue* pq);
 QDataType QueueFront(Queue* pq);
 QDataType QueueBack(Queue* pq);
+
+
+/*
+ * 
+ * 设计循环队列  生产者消费者模型  图书馆 
+ * 空间固定 k
+ * 有效数据的元素个数
+ * 看自己的设计 k-1 或者 k
+ * k 是由缺陷的  相等 可能为空 也可能满了
+ * 所以我们设计k-1个
+ */
+
+// 如何实现环状
+// 这里我们用一个 数组 注意链表可以
+// 这里超出数组长度 就回到下标 0
+
+// 存在一个对头和一个堆为
+// 相等为空
+//
+typedef int MQDataType;
+typedef struct 
+{
+
+  MQDataType* array;
+  size_t front;
+  size_t tail;  // 指向最后一个数据的下一个位置
+  size_t cap;
+} MyCircularQueue;
+
+
+MyCircularQueue* myCircularQueueCreate(int k);
+
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value);
+bool myCircularQueueDeQueue(MyCircularQueue* obj);
+
+int myCircularQueueFront(MyCircularQueue* obj);
+
+int myCircularQueueRear(MyCircularQueue* obj);
+
+bool myCircularQueueIsEmpty(MyCircularQueue* obj);
+
+bool myCircularQueueIsFull(MyCircularQueue* obj);
+
+void myCircularQueueFree(MyCircularQueue* obj);
