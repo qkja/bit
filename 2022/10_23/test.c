5f813e08d998fd32b20e5f235a18cd776aec6e7c
算法杰哥 <qkj1205@163.com>
2022-10-23 21:08:34 +0800

    基础IO

---
+++ 2022/10_23/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+
+
+
+
+
+
+//int main()
+//{
+//  while(1)
+//  {
+//    printf("hello bit");
+//    sleep(1);
+//  }
+//  return 0;
+//}
