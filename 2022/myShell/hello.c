1e47aad631a58dcb1bb1f9988f7046ba34cea7cc
算法杰哥 <qkj1205@163.com>
2022-10-29 16:58:09 +0800

    myShell更新完比

--- 2022/myShell/hello.c
+++ 2022/myShell/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-  printf("hello myshell\n");
+  printf("MYVAL:%s\n" ,getenv("MYVAL"));
+  //printf("hello myshell\n");
   return 0;
 }
--- 2022/myShell/myshell.c
+++ 2022/myShell/myshell.c
@@ -11,6 +11,18 @@
 
 char command_line[NUM];
 char* command_args[SIZE];
+char env_buffer[NUM];
+
+int ChangeDir(char* newPath)
+{
+  chdir(newPath);
+  return 0;
+}
+// 我们导入是一个地址   这里command_arg[1] 每次都更新
+void PutEnvMyShell(char* new_env)
+{
+  putenv(new_env);
+}
 
 int main()
 {
@@ -29,15 +41,53 @@ int main()
     // 这里我们用 fgets 这里获取的是 NUM - 1个字符  注意这里是C风格的字符串
     fgets(command_line, NUM, stdin);
     // 我们回车一下也是 \n 这里的字符串是不可显得,我们要去掉
+    // 这是C语言风格的
     command_line[strlen(command_line)-1] = '\0';
     
     //分割字符串 "ls -a -l"   -- > "ls" "-a" "-l"
     // 使用一些函数 strtok
     command_args[0] = strtok(command_line, SEP); // 这里得到 "ls"
     int index = 1;
+    // 加上一个选项
+    if(strcmp(command_args[0],"ls") == 0)
+      command_args[index++] = (char*)"--color=auto";
+
     // 注意我们是故意这么写的,不动得看返回值
     while(command_args[index++] = strtok(NULL, SEP));
-    
+
+    // 到这里分割字符串结束了  
+
+
+
+
+
+    // 这里还面对 一个问题  cd 这个选项  
+    // 原因是 进程具有独立性 是子进程发生了进程替换,我们修改子进程是不会影响父进程的  
+    // 所以这个cd命令不能去发生进程替换 
+    // 我们用的 cd 不是uer/bin的cd 
+    // 这里是一个chdir  更改目录函数
+    if(strcmp(command_args[0], "cd") == 0 && command_args[1] != NULL)
+    {
+      //这里就是 cd 
+      ChangeDir(command_args[1]);
+      continue;
+    }
+
+
+
+    // 这里还是没有导入成功
+    if(strcmp(command_args[0],"export") == 0 && command_args[1] != NULL)
+    {
+      // 我们需要保存 环境变量的内容
+      strcpy(env_buffer, command_args[1]);
+      PutEnvMyShell(env_buffer);
+      //PutEnvMyShell(command_args[1]);
+      continue;
+    }
+
+
+
+
     //这里我们测试一下
     //for(int i = 0; i < index; i++)
     //{
@@ -51,9 +101,11 @@ int main()
     {
       // child
       // 选择哪一个
+      // 这里我们使用的PATH里面的 命令
       execvp(command_args[0], command_args);
       exit(104);// 只要执行;了,这里替换一定失败了
     }
+
     int status = 0;
     pid_t ret = waitpid(id, &status, 0);
     if(ret > 0)
