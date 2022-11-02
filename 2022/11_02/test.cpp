9e02d713185160b22599a7c75b65b64d2f37781c
算法杰哥 <qkj1205@163.com>
2022-11-02 21:46:47 +0800

    system V

---
+++ 2022/11_02/test.cpp
@@ -0,0 +1,22 @@
+/** 
+ * Author       : Qkj
+ * Description  : system V
+ * Date         : 2022-11-02 19:16:01
+ * LastEditTime : 2022-11-02 19:20:57
+ */
+
+class A
+{
+    public:
+    
+    int a()
+    {
+
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
