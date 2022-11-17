7d181129a666a0001b6ea40cc257ec3d72c02695
算法杰哥 <qkj1205@163.com>
2022-11-17 21:18:32 +0800

    共享内存使用失败

--- 2022/11_17/Comm.hpp
+++ 2022/11_17/Comm.hpp
@@ -2,28 +2,176 @@
  * Author       : Qkj
  * Description  : 
  * Date         : 2022-11-17 16:43:52
- * LastEditTime : 2022-11-17 17:40:57
+ * LastEditTime : 2022-11-17 21:01:37
  */
+
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
+#include <cerrno>
+#include <cassert>
 #include <unistd.h>
-#include <cstdio>
-#include <errno.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+//#include "Log.hpp"
 
-#define IPC_NAME "/home/bit/104/2022/11_17"
+#define PATH_NAME "/home/bit/104/2022/11_17"
 #define PROJ_ID 0x14
 #define MEM_SIZE 4096
+
+#define FIFO_FILE ".fifo"
+
 key_t CreateKey()
 {
-  key_t key = ftok(IPC_NAME, PROJ_ID);
-  if(key < 0)
-  {
-    std::cerr<< "ftok : " << strerror(errno) << std::endl;
-    exit(1);
-  }
-  return key;
+    key_t key = ftok(PATH_NAME, PROJ_ID);
+    if(key < 0)
+    {
+        std::cerr <<"ftok: "<< strerror(errno) << std::endl;
+        exit(1);
+    }
+    return key;
 }
+
+void CreateFifo()
+{
+    umask(0);
+    if(mkfifo(FIFO_FILE, 0666) < 0)
+    {
+        std::cerr << strerror(errno) << "\n";
+        exit(2);
+    }
+}
+
+#define READER O_RDONLY
+#define WRITER O_WRONLY
+
+int Open(const std::string &filename, int flags)
+{
+    return open(filename.c_str(), flags);
+}
+
+int Wait(int fd)
+{
+    uint32_t values = 0;
+    ssize_t s = read(fd, &values, sizeof(values));
+    return s;
+}
+
+int Signal(int fd)
+{
+    uint32_t cmd = 1;
+    write(fd, &cmd, sizeof(cmd));
+}
+
+int Close(int fd, const std::string filename)
+{
+    close(fd);
+    unlink(filename.c_str());
+}
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// #include <string>
+// #include <cstring>
+// #include <unistd.h>
+// #include <cstdio>
+// #include <sys/types.h>
+// #include <errno.h>
+// #include <stdlib.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <fcntl.h>
+// #include <sys/ipc.h>
+// #include <sys/shm.h>
+// #include <assert.h>
+// //#include "Log.hpp"
+
+// #define IPC_NAME "/home/bit/104/2022/11_17"
+// #define PROJ_ID 0x14
+// #define MEM_SIZE 4096
+// #define FIFO_FILE ".fifo"
+
+// #define READER O_RDONLY
+// #define WRITER O_WRONLY
+
+// key_t CreateKey()
+// {
+//   key_t key = ftok(IPC_NAME, PROJ_ID);
+//   if(key < 0)
+//   {
+//     std::cerr<< "ftok : " << strerror(errno) << std::endl;
+//     exit(1);
+//   }
+//   return key;
+// }
+
+
+// // 创建管道
+// void CreateFifo()
+// {
+//   if(mkfifo(FIFO_FILE, 0666) < 0)
+//   {
+//     std::cerr<< "mkfifo fail" << std::endl;
+//     exit(2);
+//   }
+// }
+
+// int Open(const std::string& filename, int falgs)
+// {
+//   //printf("qqq\n");
+//   return open(filename.c_str(), falgs);
+//   //printf("qqq\n");
+
+//   //return fd;
+// }
+
+
+// int Wait(int fd)
+// {
+//   uint32_t values = 0;
+//   ssize_t s = read(fd, &values, sizeof(values));
+//   if(s <= 0)
+//     return 0;
+//   //assert(s == sizeof(values));
+//   return s;
+// }
+
+// void Signal(int fd)
+// {
+//    uint32_t cmd = 0;
+//    write(fd, &cmd, sizeof(cmd));
+// }
+
+// void Close(int fd, const std::string& name)
+// {
+//   close(fd);
+//   unlink(name.c_str());
+
+// }
+
+
+
+
+
+// int OpenForRead(std::string& filename)
+// {
+//   int fd = open(filename.c_str(), O_RDONLY);
+//   return fd;
+// }
+
+// int OpenForWrite(std::string& filename)
+// {
+//   int fd = open(filename.c_str(), O_WRONLY);
+//   return fd;
+// }
--- 2022/11_17/IpcShmCli.cc
+++ 2022/11_17/IpcShmCli.cc
@@ -2,34 +2,131 @@
  * Author       : Qkj
  * Description  : 
  * Date         : 2022-11-17 16:33:30
- * LastEditTime : 2022-11-17 19:07:31
+ * LastEditTime : 2022-11-17 21:03:50
  */
+
+
 #include "Comm.hpp"
-// 充当 使用共享内存的角色
 #include "Log.hpp"
+
+#include <cstdio>
+#include <unistd.h>
+
 using namespace std;
+// 充当使用共享内存的角色
 int main()
 {
-  key_t key = CreateKey();
+    int fd = Open(FIFO_FILE, WRITER);
+    // 创建相同的key值
+    key_t key = CreateKey();
+    Log() << "key: " << key << "\n";
 
-  int shmid = shmget(key, MEM_SIZE, IPC_CREAT);
-  if(shmid < 0)
-  {
-    Log() << strerror(errno) << endl;
-    exit(2);
-  }
+    // 获取共享内存
+    int shmid = shmget(key, MEM_SIZE, IPC_CREAT);
+    if (shmid < 0)
+    {
+        Log() << "shmget: " << strerror(errno) << "\n";
+        return 2;
+    }
 
-  // 挂接
-  char* str = (char*)shmat(shmid, nullptr, 0);
-  if(str == nullptr)
-  {}  // 判断一下
+    // 挂接
+    char *str = (char*)shmat(shmid, nullptr, 0);
 
-  // 用它
+    while(true)
+    {
+        printf("Please Enter# ");
+        fflush(stdout);
+        ssize_t s = read(0, str, MEM_SIZE);
+        if(s > 0)
+        {
+            str[s] = '\0';
+        }
+        Signal(fd);
+    }
+    shmdt(str);
 
-
-  sleep(1);
-
-  shmdt(str);  // 参数就是放回置
-  // 这类不做删除   我们只是 使用
-  return 0;
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+// #include "Comm.hpp"
+// // 充当 使用共享内存的角色
+// #include "Log.hpp"
+// using namespace std;
+// int main()
+// {
+//   // printf("111\n");
+//   // fflush(stdout);
+//   //CreateFifo();
+
+//   int fd = Open(FIFO_FILE, WRITER);
+//   // printf("222\n");
+//   // fflush(stdout);
+//   assert(fd >= 0);
+//   key_t key = CreateKey();
+//   int shmid = shmget(key, MEM_SIZE, IPC_CREAT);
+
+//   // printf("333\n");
+//   // fflush(stdout);
+//   if(shmid < 0)
+//   {
+//     Log() << strerror(errno) << endl;
+//     exit(2);
+//   }
+  
+
+//   // 挂接
+//   char* str = (char*)shmat(shmid, nullptr, 0);
+//   if(str == nullptr)
+//   {}  // 判断一下
+//   //sleep(10);
+//   // 用它
+//   //int cnt = 0;
+
+
+//   while(true)
+//   {
+//     printf("请输入: ");
+//     fflush(stdout);
+//     size_t s = read(0, str, MEM_SIZE);
+//     if(s > 0)
+//     {
+//       // abc\n
+//       str[s-1] = '\0';
+//     }
+//     Signal(fd);
+//   }
+
+
+//   // while(true)
+//   // {
+//   //   printf("请输入: ");
+//   //   fflush(stdout);
+//   //   size_t s = read(0, str, MEM_SIZE);
+//   //   if(s > 0)
+//   //   {
+//   //     // abc\n
+//   //     str[s-1] = '\0';
+//   //   }
+//   // }
+//   // while(cnt < 26)
+//   // {
+//   //   str[cnt] = 'A' + cnt;
+//   //   ++cnt;
+//   //   str[cnt] = '\0';
+//   //   sleep(1);
+
+//   // }
+//   //sleep(1);
+
+//   shmdt(str);  // 参数就是放回置
+//   // 这类不做删除   我们只是 使用
+//   return 0;
+// }
