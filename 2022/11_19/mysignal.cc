913d7d79fc95f8364b4283ad210dfa3f4cb2e51e
算法杰哥 <qkj1205@163.com>
2022-11-19 14:24:58 +0800

    捕捉动作

---
+++ 2022/11_19/mysignal.cc
@@ -0,0 +1,21 @@
+/** 
+ * Author       : Qkj
+ * Description  : 
+ * Date         : 2022-11-19 14:10:29
+ * LastEditTime : 2022-11-19 14:22:50
+ */
+#include <signal.h> 
+// 认识另外的信号捕捉
+int main()
+{
+  
+
+// struct sigaction {
+//                void     (*sa_handler)(int);
+//                void     (*sa_sigaction)(int, siginfo_t *, void *);
+//                sigset_t   sa_mask;
+//                int        sa_flags;
+//                void     (*sa_restorer)(void);
+//            };
+  return 0;
+}
