45e0288e684f272f6110ddcb61620ae77549c817
算法杰哥 <qkj1205@163.com>
2022-11-27 16:03:48 +0800

    udp套接字

---
+++ 2022/11_27/udp/udpClient.cc
@@ -0,0 +1,10 @@
+/** 
+ * Author       : Qkj
+ * Description  : 
+ * Date         : 2022-11-27 10:39:49
+ * LastEditTime : 2022-11-27 10:43:14
+ */
+int main()
+{
+  return 0;
+}
---
+++ 2022/11_27/udp/udpServer.cc
@@ -0,0 +1,31 @@
+/**
+ * Author       : Qkj
+ * Description  : udp套接字
+ * Date         : 2022-11-27 10:39:59
+ * LastEditTime : 2022-11-27 10:43:20
+ */
+#include <sys/types.h>
+#include <sys/socket.h>
+
+class udpServer
+{
+public:
+  udpServer()
+  {
+  }
+  ~udpServer()
+  {
+  }
+
+public:
+void start(){};
+void init(){};
+private:
+int _sockfd;
+};
+
+int main()
+{
+  //int fd = socket(A)
+  return 0;
+}
