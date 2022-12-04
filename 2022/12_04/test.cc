093d798072d0dc6e0470fdb599bc457def7fdccd
算法杰哥 <qkj1205@163.com>
2022-12-04 23:38:50 +0800

    文件描述符结尾

--- 2022/12_04/test.cc
+++ 2022/12_04/test.cc
@@ -11,26 +11,130 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-int main()
-{
-  umask(0);
-  int fd = open("log.txt",  O_WRONLY | O_CREAT | O_TRUNC, 0666);
-  int val = 0x41424344;
-  char * msg = "abcd";
-  int len = strlen(msg);
-  write(fd,&val, 4);
-  close(fd);
+// void read(struct file *filep, int fd....)
+// {
+//   // 逻辑代码
+// }
 
-  return 0;
-}
+// void write(struct file *filep, int fd....)
+// {
+//   // 逻辑代码
+// }
+// struct file
+// {
+//   // 对象属性
 
+//   //函数指针   这个是类型
+//   void (*read)(struct file *filep, int fd....);
+//   void (*write)(struct file *filep, int fd....);
+// }
 
+// struct file
+// {
 
+//   //文件 属性 + 内容
 
+//   struct file *prev;
+//   struct file *next;
+// };
 
+// int main()
+// {
+//   umask(0);
 
+//   const char *msg = "123456\n";
 
+//   write(1, msg, strlen(msg));
+//   return 0;
+// }
+// int main()
+// {
+//   umask(0);
+//   char buffer[128];
+//   // 从 fd = 0的是文件中去读,读到内容放在 buffer中
+//   ssize_t s = read(0, buffer, sizeof(buffer) - 1);
 
+//   if (s > 0)
+//   {
+//     buffer[s] = '\0';
+//     printf("输出 %s", buffer);
+//   }
+//   return 0;
+// }
+// int main()
+// {
+//   umask(0);
+
+//   printf("stdin   : %d\n", stdin->_fileno);
+//   printf("stdout  : %d\n", stdout->_fileno);
+//   printf("stderr  : %d\n", stderr->_fileno);
+
+//   return 0;
+// }
+
+// int main()
+// {
+//   umask(0);
+//   FILE* pf1 = fopen("log1.txt","w");
+//   FILE* pf2 = fopen("log2.txt","w");
+//   FILE* pf3 = fopen("log3.txt","w");
+
+//   printf("pf1 : %d\n", pf1->_fileno);
+//   printf("pf2 : %d\n", pf2->_fileno);
+//   printf("pf3 : %d\n", pf3->_fileno);
+
+//   return 0;
+// }
+
+// int main()
+// {
+//   umask(0);
+//   int fd = open("log.txt", O_RDONLY, 0666);
+
+//   char buffer[128];
+//   ssize_t s = read(fd, buffer, sizeof(buffer) - 1);
+//   if (s > 0)
+//   {
+//     buffer[s] = '\0'; // 这个注意下标
+//     printf("%d %s", s, buffer);
+//   }
+//   return 0;
+// }
+
+// int main()
+// {
+//   umask(0);
+//   int fd = open("file.txt", O_RDONLY, 0666);
+
+//   if (fd < 0)
+//   {
+//     printf("%s\n", strerror(errno));
+//     exit(1);
+//   }
+
+//   printf("fd %d\n", fd);
+//   char buffer[128];
+//   ssize_t s = read(fd, buffer, sizeof(buffer) - 1);
+//   if (s > 0)
+//   {
+//     buffer[s] = '\0'; // 这个注意下标
+//     printf("%s\n", buffer);
+//   }
+//   return 0;
+// }
+
+// int main()
+// {
+//   umask(0);
+//   int fd = open("log.txt",  O_WRONLY | O_CREAT | O_TRUNC, 0666);
+//   int val = 0x41424344;
+//   char * msg = "abcd";
+//   int len = strlen(msg);
+//   write(fd,&val, 4);
+//   close(fd);
+
+//   return 0;
+// }
 
 // int main()
 // {
