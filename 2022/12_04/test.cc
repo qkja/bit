1e4a23e136a9dfca5bdfda2e935e89781fe4c977
算法杰哥 <qkj1205@163.com>
2022-12-04 15:43:21 +0800

    write 写入是按照一个字节一个字节来的 -- 个人理解

--- 2022/12_04/test.cc
+++ 2022/12_04/test.cc
@@ -11,19 +11,52 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+int main()
+{
+  umask(0);
+  int fd = open("log.txt",  O_WRONLY | O_CREAT | O_TRUNC, 0666);
+  int val = 0x41424344;
+  char * msg = "abcd";
+  int len = strlen(msg);
+  write(fd,&val, 4);
+  close(fd);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+// int main()
+// {
+//   umask(0);
+//   int fd = open("log.txt", O_WRONLY | O_CREAT, 0666);
+
+//   const char *str = "1234567";
+//   int len = strlen(str);
+
+//   write(fd, str, len);
+
+//   return 0;
+// }
 
 // int main()
 // {
 //   umask(0);
 //   int fd = open("log.txt",  O_WRONLY | O_CREAT | O_TRUNC, 0666);
-//   int val = 0x11223344;
+//   int val = 0x41424344;
 //   //
 //   char * msg = "abcd";
 //   int len = strlen(msg);
 //   //write(fd, msg, len);
-//   write(fd,&val, 1);
-//   val = 'D';
-//   std::cout << val<< std::endl;
+//   write(fd,&val, 4);
+//   // val = 'D';
+//   // std::cout << val<< std::endl;
 //   close(fd);
 
 //   return 0;
@@ -85,34 +118,34 @@
 //   return 0;
 // }
 
-#define PRINT_A 0x01 // 0000 0001
-#define PRINT_B 0x02 // 0000 0010
-#define PRINT_C 0x04 // 0000 0100
-#define PRINT_D 0x08 // 0000 1000
-#define PRINT_DEL 0x00
-void Show(int flags)
-{
-  if (flags & PRINT_A)
-    printf("hello A\n");
-  if (flags & PRINT_B)
-    printf("hello B\n");
-  if (flags & PRINT_C)
-    printf("hello C\n");
-  if (flags & PRINT_D)
-    printf("hello D\n");
-  if (flags == PRINT_DEL)
+// #define PRINT_A 0x01 // 0000 0001
+// #define PRINT_B 0x02 // 0000 0010
+// #define PRINT_C 0x04 // 0000 0100
+// #define PRINT_D 0x08 // 0000 1000
+// #define PRINT_DEL 0x00
+// void Show(int flags)
+// {
+//   if (flags & PRINT_A)
+//     printf("hello A\n");
+//   if (flags & PRINT_B)
+//     printf("hello B\n");
+//   if (flags & PRINT_C)
+//     printf("hello C\n");
+//   if (flags & PRINT_D)
+//     printf("hello D\n");
+//   if (flags == PRINT_DEL)
 
-    printf("hello default\n");
-}
+//     printf("hello default\n");
+// }
 
-int main()
-{
-  Show(PRINT_DEL);
-  printf("\n\n");
+// int main()
+// {
+//   Show(PRINT_DEL);
+//   printf("\n\n");
 
-  Show(PRINT_A);
-  printf("\n\n");
+//   Show(PRINT_A);
+//   printf("\n\n");
 
-  Show(PRINT_B | PRINT_C | PRINT_D);
-  return 0;
-}
+//   Show(PRINT_B | PRINT_C | PRINT_D);
+//   return 0;
+// }
