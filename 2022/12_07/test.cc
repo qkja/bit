1229489ade8a2ec84a953c5e88e3f17e63429e31
算法杰哥 <qkj1205@163.com>
2022-12-07 18:22:33 +0800

    提交代码

---
+++ 2022/12_07/test.cc
@@ -0,0 +1,84 @@
+/*
+ * @Author: 算法杰哥 qkj1205@163.com
+ * @Date: 2022-12-07 11:05:58
+ * @LastEditors: 算法杰哥 qkj1205@163.com
+ * @LastEditTime: 2022-12-07 11:05:59
+ * @FilePath: /12_07/test.cc
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// /**
+//  * User: Qkj
+//  * Description:
+//  * Date: 2022-12-06
+//  * Time: 19:01
+//  */
+// #include <stdio.h>
+// #include <iostream>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <fcntl.h>
+// #include <cstring>
+// #include <errno.h>
+
+// using namespace std;
+// void my_perror(const char *p)
+// {
+//   printf("%s: %s\n", p, strerror(errno));
+// }
+// int main()
+// {
+//   int fd = open("log.txt", O_RDONLY); // 必然失败,没有这个函数
+
+//   if (fd < 0)
+//   {
+//     // 失败后 errno会被设置
+//     my_perror("open");
+//     perror("open");
+//   }
+//   return 0;
+// }
+// // int main()
+// // {
+
+// //  perror("hello perror 2");
+// //   return 0;
+// // }
+
+// // int main()
+// // {
+
+// //   // stdout
+// //   printf("hello printf 1\n"); // 1
+// //   fprintf(stdout, "hello fprintf 1\n");
+// //   fputs("hello fputs 1\n", stdout);
+
+// //   // stderr
+
+// //   fprintf(stderr, "hello fprintf 2\n");
+// //   fputs("hello fputs 2\n", stderr);
+// //   perror("hello perror 2");
+
+// //   // cout
+// //   cout << "hello cout 1" << endl;
+
+// //   // cerr
+// //   cerr << "hello cerr 2" << endl;
+// //   return 0;
+// // }
+
+// // int main()
+// // {
+
+// //   perror("hello perror 2");
+// //   return 0;
+// // }
+#define ESC /*
+#define A */
+
+int main()
+{
+  ESC;
+  //A;
+
+  return 0;
+}
