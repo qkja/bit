be7feef4b9481640f451af988817445debd6c489
算法杰哥 <qkj1205@163.com>
2022-12-12 21:44:43 +0800

    命名管道 文件必须在意另一个进程中打开,创建文件之进程才会执行

--- 2022/12_11/fifo/clientFifo.cc
+++ 2022/12_11/fifo/clientFifo.cc
@@ -5,37 +5,48 @@
  * LastEditTime : 2022-12-11 14:14:18
  */
 #include "comm.h"
-
 int main()
 {
-  int fd = open(IPC_PATH, O_WRONLY);
-  if (fd < 0)
+  printf("你是否要打开命名管道 1/0: ");
+  fflush(stdout);
+  int ret = 0;
+  scanf("%d", &ret);
+  if (ret == 1)
   {
-    std::cerr << "open : " << errno << std::endl;
-    return 1;
+    open(IPC_PATH, O_WRONLY);
   }
-#define NUM 1024
-  char line[NUM];
-  while (true)
-  {
-    std::cout << "请输入你的消息: ";
-    fflush(stdout);
-    memset(line, '\0', sizeof(line));
-
-    if (fgets(line, sizeof(line) - 1, stdin) != NULL)
-    {
-      // fgets 读取是    abc\n\0  会自动添加'\0' -- C语言接口
-
-      line[strlen(line) - 1] = '\0';
-      write(fd, line, strlen(line));
-    }
-    else
-    {
-      break;
-    }
-  }
-  close(fd);
-  std::cout << "客户端退出了" << std::endl;
-
   return 0;
 }
+// int main()
+// {
+//   int fd = open(IPC_PATH, O_WRONLY);
+//   if (fd < 0)
+//   {
+//     std::cerr << "open : " << errno << std::endl;
+//     return 1;
+//   }
+// #define NUM 1024
+//   char line[NUM];
+//   while (true)
+//   {
+//     std::cout << "请输入你的消息: ";
+//     fflush(stdout);
+//     memset(line, '\0', sizeof(line));
+
+//     if (fgets(line, sizeof(line) - 1, stdin) != NULL)
+//     {
+//       // fgets 读取是    abc\n\0  会自动添加'\0' -- C语言接口
+
+//       line[strlen(line) - 1] = '\0';
+//       write(fd, line, strlen(line));
+//     }
+//     else
+//     {
+//       break;
+//     }
+//   }
+//   close(fd);
+//   std::cout << "客户端退出了" << std::endl;
+
+//   return 0;
+// }
