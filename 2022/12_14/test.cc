c5fc02f61988e9e16aa61242ab62c7792329a9d3
算法杰哥 <qkj1205@163.com>
2022-12-18 19:12:40 +0800

    信号结束

--- 2022/12_14/test.cc
+++ 2022/12_14/test.cc
@@ -8,9 +8,10 @@ void handler(int signo)
 {
   cout << "我是一个进程 pid " << getpid() << ",刚刚获取了一个信号: " << signo << endl;
 }
+
 void showPending(const sigset_t &pendings)
 {
-  // 如何显式
+  // 如何显式~
   for (size_t i = 1; i <= 31; i++)
   {
     if (sigismember(&pendings, i) == 1)
@@ -25,40 +26,40 @@ void showPending(const sigset_t &pendings)
   cout << endl;
 }
 
-// int main()
-// {
-//   cout << "pid : " << getpid() << std::endl;
-//   sigset_t bsig, obsig;
+int main()
+{
+  cout << "pid : " << getpid() << std::endl;
+  sigset_t bsig, obsig;
 
-//   sigemptyset(&bsig);  // 清空当前信号集
-//   sigemptyset(&obsig); // 清空当前信号集
+  sigemptyset(&bsig);  // 清空当前信号集
+  sigemptyset(&obsig); // 清空当前信号集
 
-//   for (int i = 1; i <= 31; i++)
-//   {
-//     sigaddset(&bsig, i);
-//     signal(i, handler);
-//   }
+  for (int i = 1; i <= 31; i++)
+  {
+    sigaddset(&bsig, i);
+    signal(i, handler);
+  }
 
-//   sigprocmask(SIG_SETMASK, &bsig, &obsig);
-//   sigset_t pendings;
-//   int cnt = 0;
-//   while (true)
-//   {
-//     sigemptyset(&pendings); // 清空当前信号集
-//     if (sigpending(&pendings) == 0)
-//     {
-//       showPending(pendings);
-//     }
-//     sleep(1);
-//     cnt++;
-//     if (cnt == 15)
-//     {
-//       sigset_t s;
-//       sigemptyset(&s);
-//       sigaddset(&s, 2);
-//       sigprocmask(SIG_UNBLOCK, &s, nullptr);
-//     }
-//   }
+  sigprocmask(SIG_SETMASK, &bsig, &obsig);
+  sigset_t pendings;
+  int cnt = 0;
+  while (true)
+  {
+    sigemptyset(&pendings); // 清空当前信号集
+    if (sigpending(&pendings) == 0)
+    {
+      showPending(pendings);
+    }
+    sleep(1);
+    cnt++;
+    if (cnt == 15)
+    {
+      sigset_t s;
+      sigemptyset(&s);
+      sigaddset(&s, 2);
+      sigprocmask(SIG_UNBLOCK, &s, nullptr);
+    }
+  }
 
-//   return 0;
-// }
+  return 0;
+}
