e75f6d576f4209e00378b8ceaa28931e4353993e
算法杰哥 <qkj1205@163.com>
2023-01-01 21:32:12 +0800

    认识socket

--- 2023/01_01/udpServer.cc
+++ 2023/01_01/udpServer.cc
@@ -1,7 +1,10 @@
-#include <sys/types.h> /* See NOTES */
-#include <sys/socket.h>
 #include <iostream>
 #include <string>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 using namespace std;
 
 class udpServer
@@ -14,9 +17,12 @@ public:
   void init()
   {
   }
+  void start()
+  {
+  }
 
 private:
-  /* data */
+  int _socketFd;
 };
 
 int main()
@@ -24,9 +30,19 @@ int main()
   // 创建套接字
   // int socket(int domain, int type, int protocol);
   // 本地或者域间 数据包/字节序  协议类型--网络中直接为0
+  // 重要的 返回值就是一个文件描述符  -- 就是打开网卡这个文件 -- 我们这么认为,实际是不对的
 
+  // udpServer ser;
+  // ser.init();
+  // ser.start();
+  // 或者PF_INT   数据报 -- udp SOCK_DGRAM
+  int fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (fd < 0)
+  {
+    cout << strerror(errno) << endl;
+    return 0;
+  }
 
-  udpServer ser;
-  ser.init();
+  cout << fd << endl;
   return 0;
 }
