85b817557ebfefc3f9959ec365e4ed050f8285bd
算法杰哥 <qkj1205@163.com>
2023-01-22 21:43:02 +0800

    重构 初识C语言结束

---
+++ 2023/01_22/test.c
@@ -0,0 +1,23 @@
+/**
+* User: Qkj
+* Description: 
+* Date: 2023-01-22
+* Time: 19:01
+*/
+#include<stdio.h>
+
+extern int g_val = 10;
+
+int main()
+{
+	printf("%d", g_val);
+	return 0;
+}
+
+
+//int main()
+//{
+//	int a;
+//	printf("%d\n", a);
+//	return 0;
+//}
