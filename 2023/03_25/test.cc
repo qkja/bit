51501d93b0a4860db3b87686e26b02bfccc8e94f
算法杰哥 <qkj1205@163.com>
2023-03-25 22:36:59 +0800

    unique_ptr 可以接受右值,以达到资源转移的目的

---
+++ 2023/03_25/test.cc
@@ -0,0 +1,23 @@
+/**
+* User: Qkj
+* Description: 
+* Date: 2023-03-25
+* Time: 22:01
+*/
+#include <memory>
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//   unique_ptr<int> p(new int(1));
+//  // cout << *p << endl;
+//   unique_ptr<int>& p2 = p;
+//   //cout << *p2 << endl;
+//   unique_ptr<int> p3(new int(2));
+//   p3 = std::move(p);
+//   //cout << *p2 << endl;
+//   cout << *p3 << endl;
+
+//   return 0;
+// }
