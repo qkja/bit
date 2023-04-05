072c382f7d7ec4d0d70cb57ce3cb2197d62884d0
算法杰哥 <qkj1205@163.com>
2023-04-05 21:15:23 +0800

    实现Rector

---
+++ 2023/04_05/Reactor/Epoller.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <sys/epoll.h>
+
+#include "Log.hpp"
+
+class Epoller
+{
+public:
+  static const int gszie = 128;
+
+public:
+  // 创建 句柄
+  static int CreateEpoller()
+  {
+    int epfd = epoll_create(gszie); // gszie 随便写
+    if (-1 == epfd)
+    {
+      logMessage(FATAL, "CreateEpoller() : %d : %s", errno, strerror(errno));
+      exit(3);
+    }
+    return epfd;
+  }
+
+  // 添加文件描述符到 epfd中
+  static bool AddEvent(int epfd, int sock, uint32_t event)
+  {
+    struct epoll_event ev;
+    ev.events = event;
+    ev.data.fd = sock;
+    int n = epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &ev);
+    return n == 0;
+  }
+
+  // 拿到就绪事件
+
+  static int LoopOnce(int epfd, struct epoll_event revs[], int num)
+  {
+    int n = epoll_wait(epfd, revs, num, -1); // 阻塞式
+    if (-1 == n)
+    {
+      logMessage(FATAL, "epoll_wait %d %s", errno, strerror(errno));
+    }
+    return n;
+  }
+};
