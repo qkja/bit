8f2f41d1918db3bbb485da4dc33666d845270dae
算法杰哥 <qkj1205@163.com>
2023-04-17 15:24:57 +0800

    算法课 第8课

--- 2023/04_17/test.cc
+++ 2023/04_17/test.cc
@@ -131,41 +131,208 @@ using std::vector;
 //   }
 // };
 
-class Solution
-{
-  vector<vector<int>> arr = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 上下左右
-public:
-  void Dfs(vector<vector<int>> &image, int row, int col,
-           vector<vector<int>> &book, int x, int y,
-           int newColor, int oldColor)
-  {
-    image[x][y] = newColor;
-    book[x][y] = false;
+// class Solution
+// {
+//   vector<vector<int>> arr = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 上下左右
+// public:
+// void Dfs(vector<vector<int>> &image, int row, int col,
+//          vector<vector<int>> &book, int x, int y,
+//          int newColor, int oldColor)
+// {
+//   image[x][y] = newColor;
+//   book[x][y] = false;
 
-    for (size_t i = 0; i < 4; i++)
-    {
-      // 四个位置
-      int newX = x + arr[i][0];
-      int newY = y + arr[i][1];
+//   for (size_t i = 0; i < 4; i++)
+//   {
+//     // 四个位置
+//     int newX = x + arr[i][0];
+//     int newY = y + arr[i][1];
 
-      if (!(newX >= 0 && newX < row && newY >= 0 && newY < col))
-        continue;
-      if (image[newX][newY] == oldColor && book[newX][newY] == true)
-      {
-        Dfs(image, row, col, book, newX, newY, newColor, oldColor);
-      }
-    }
-  }
+//     if (!(newX >= 0 && newX < row && newY >= 0 && newY < col))
+//       continue;
+//     if (image[newX][newY] == oldColor && book[newX][newY] == true)
+//     {
+//       Dfs(image, row, col, book, newX, newY, newColor, oldColor);
+//     }
+//   }
+// }
 
-  vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
-  {
-    if (image.empty())
-      return image;
-    int old = image[sr][sc];
-    int row = image.size();
-    int col = image[0].size();
-    vector<vector<int>> book(row, vector<int>(col, true));
-    Dfs(image, row, col, book, sr, sc, color, old);
-    return image;
-  }
-};
+//   vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
+//   {
+//     if (image.empty())
+//       return image;
+//     int old = image[sr][sc];
+//     int row = image.size();
+//     int col = image[0].size();
+//     vector<vector<int>> book(row, vector<int>(col, true));
+//     Dfs(image, row, col, book, sr, sc, color, old);
+//     return image;
+//   }
+// };
+#include <unordered_map>
+#include <unordered_set>
+// class Solution
+// {
+// public:
+//   vector<vector<int>> arr = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 上下左右
+
+//   void Dfs(vector<vector<char>> &image, int row, int col,
+//            vector<vector<int>> &book, int x, int y,
+//            char newColor, char oldColor)
+//   {
+//     image[x][y] = newColor;
+//     book[x][y] = false;
+
+//     for (size_t i = 0; i < 4; i++)
+//     {
+//       // 四个位置
+//       int newX = x + arr[i][0];
+//       int newY = y + arr[i][1];
+
+//       if (!(newX >= 0 && newX < row && newY >= 0 && newY < col))
+//         continue;
+//       if (image[newX][newY] == oldColor && book[newX][newY] == true)
+//       {
+//         Dfs(image, row, col, book, newX, newY, newColor, oldColor);
+//       }
+//     }
+//   }
+//   void solve(vector<vector<char>> &board)
+//   {
+//     std::unordered_map<int, std::pair<int, int>> s;
+//     int row = board.size();
+//     int col = board[0].size();
+//     int index = 0;
+//     for (int i = 0; i < board[0].size(); i++)
+//     {
+//       if (board[0][i] == '0')
+//         s[index++] = std::make_pair(0, i);
+//       if (board[row - 1][i])
+//         s[index++] = std::make_pair(col - 1, i);
+//     }
+
+//     for (int i = 1; i < row - 1; i++)
+//     {
+//       if (board[i][0] == '0')
+//         s[index++] = std::make_pair(i, 0);
+//       if (board[i][col - 1])
+//         s[index++] = std::make_pair(i, col - 1);
+//     }
+
+//     vector<vector<int>> book(row, vector<int>(col, true));
+//     auto it = s.begin();
+//     while (it != s.end())
+//     {
+//       if (board[it->second.first][it->second.second] == 'O' && book[it->second.first][it->second.second])
+//         Dfs(board, row, col, book, it->second.first, it->second.second, 'o', 'a');
+//       it++;
+//     }
+
+//     for (int i = 0; i < row; i++)
+//     {
+//       for (size_t j = 0; j < col; j++)
+//       {
+//         if (board[i][j] == 'a')
+//           board[i][j] = 'o';
+//         else
+//           board[i][j] = 'x';
+//       }
+//     }
+//   }
+// };
+
+// struct hashe
+// {
+//   int operator()(const std::pair<int, int>& p)
+//   {
+//     return p.first + p.second;
+//   }
+// };
+
+// int main()
+// {
+//   std::unordered_set<std::pair<int, int>, hashe()> s;
+//   s.insert({1, 1});
+//   s.insert({1, 1});
+//   s.insert({2, 2});
+//   auto it = s.begin();
+//   while (it != s.end())
+//   {
+//     cout << it->first << " " << it->second << endl;
+//     it++;
+//   }
+
+//   return 0;
+// }
+
+// class Solution
+// {
+// public:
+//   vector<vector<int>> arr = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 上下左右
+
+//   void Dfs(vector<vector<char>> &image, int row, int col,
+//            vector<vector<bool>> &book, int x, int y,
+//            char newColor, char oldColor)
+//   {
+//     image[x][y] = newColor;
+//     book[x][y] = false;
+
+//     for (size_t i = 0; i < 4; i++)
+//     {
+//       // 四个位置
+//       int newX = x + arr[i][0];
+//       int newY = y + arr[i][1];
+
+//       if (!(newX >= 0 && newX < row && newY >= 0 && newY < col))
+//         continue;
+//       if (image[newX][newY] == oldColor && book[newX][newY] == true)
+//       {
+//         Dfs(image, row, col, book, newX, newY, newColor, oldColor);
+//       }
+//     }
+//   }
+//   void solve(vector<vector<char>> &board)
+//   {
+//     if (board.empty())
+//       return;
+//     std::unordered_map<int, std::pair<int, int>> s;
+//     int row = board.size();
+//     int col = board[0].size();
+//     int index = 0;
+//     for (int i = 0; i < col; i++)
+//     {
+//       if (board[0][i] == 'O')
+//         s[index++] = std::make_pair(0, i);
+//       if (board[row - 1][i] == 'O')
+//         s[index++] = std::make_pair(row - 1, i);
+//     }
+
+//     for (int i = 1; i < row - 1; i++)
+//     {
+//       if (board[i][0] == 'O')
+//         s[index++] = std::make_pair(i, 0);
+//       if (board[i][col - 1] == 'O')
+//         s[index++] = std::make_pair(i, col - 1);
+//     }
+
+//     vector<vector<bool>> book(row, vector<bool>(col, true));
+//     auto it = s.begin();
+//     while (it != s.end())
+//     {
+//       if (board[it->second.first][it->second.second] == 'O')
+//         Dfs(board, row, col, book, it->second.first, it->second.second, 'A', 'O');
+//       it++;
+//     }
+
+//     for (int i = 0; i < row; i++)
+//     {
+//       for (size_t j = 0; j < col; j++)
+//       {
+//         if (board[i][j] == 'A')
+//           board[i][j] = 'O';
+//         else
+//           board[i][j] = 'X';
+//       }
+//     }
+//   }
+// };
